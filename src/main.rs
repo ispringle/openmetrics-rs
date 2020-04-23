41678c91240b9110ef9f65dee5214024a3d3c5f0
Ian S. Pringle <ipringle2@liberty.edu>
2020-04-23 18:35:25 -0400

    Turning parsed data into consumable data
    Creating a Metrics struct which contains a hashmap of MetricGroups, each
    with the data unique to that specific metric name.

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,60 @@ extern crate pest_derive;
 use std::fs;
 use pest::Parser;
 
+mod metrics;
+
 #[derive(Parser)]
 #[grammar = "metric.pest"]
 pub struct MetricParser;
 
 fn main() {
     let unparsed_file = fs::read_to_string("test.prom").expect("cannot read file!");
-    let metrics = MetricParser::parse(Rule::metrics, &unparsed_file)
+    let raw_metrics = MetricParser::parse(Rule::metrics, &unparsed_file)
         .expect("unsuccessful parse 😥")
         .next().unwrap();
 
-    println!("{:?}", metrics);
+    //let mut metrics: HashMap<String, metrics::MetricGroup> = HashMap::new();
+    let mut metrics = metrics::Metrics::new();
+
+    for raw_metric in raw_metrics.into_inner() {
+        for line in raw_metric.into_inner() {
+            match line.as_rule() {
+                Rule::helpLine => {
+                    let mut inner = line.into_inner();
+                    metrics.entry(inner.next().unwrap().as_str().to_string())
+                        .and_modify(
+                            |m| m.help = inner.next().unwrap().as_str().to_string()
+                        )
+                        .or_insert(
+                            metrics::MetricGroup::new_with_help(
+                                inner.next().unwrap().as_str()
+                            )
+                        );
+                }
+                Rule::typeLine => {
+                    let mut inner = line.into_inner();
+                    metrics.entry(inner.next().unwrap().as_str().to_string())
+                        .and_modify(
+                            |m| m.r#type = match inner.next().unwrap().as_str() {
+                                "counter" => metrics::MetricType::COUNTER,
+                                "gauge" => metrics::MetricType::GAUGE,
+                                "histogram" => metrics::MetricType::HISTOGRAM,
+                                "summary" => metrics::MetricType::SUMMARY,
+                                _ => metrics::MetricType::NONE,
+                            })
+                        .or_insert(
+                            match inner.next() {
+                                Some(t) => metrics::MetricGroup::new_with_type(t.as_str()),
+                                _ => metrics::MetricGroup::new_with_type(""),
+                            });
+                }
+                _ => {
+        println!("{:#?}", line.into_inner());
+                }
+
+            }
+        }
+    }
+
+println!("{:#?}", metrics);
 }
